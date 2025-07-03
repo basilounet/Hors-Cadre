da1f6f2c8cea3731a313e7db2bb0fe5b11573ee2
Baba <45125301+basilounet@users.noreply.github.com>
2025-07-03 15:30:01 +0200

    Added Run animations in all directions, Added Mbatty player models & chair model & texxtures

--- Assets/Scripts/PlayerMouvment.cs
+++ Assets/Scripts/PlayerMouvment.cs
@@ -10,37 +10,41 @@ public class PlayerMouvment : MonoBehaviour
 
     private InputAction		_moveAction;
     private InputAction		_jumpAction;
+    private InputAction		_SprintAction;
 
     private Vector2     	_moveInput;
     private Rigidbody		_rb;
 	private ParticleSystem	_jumpParticles;
+	private	bool			_isRunning;
 
-	[SerializeField] private float	_moveSpeed = 5f;
+	[SerializeField] private float	_moveSpeed = 40f;
+	[SerializeField] private float	_runSpeed = 80f;
 	[SerializeField] private float	_movementSmoothing = 10f; // Higher values = more responsive, lower = smoother
 	[SerializeField] private bool	_isGrounded = true;
 	[SerializeField] private float	_jumpForce = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        InputAction.FindActionMap("Player").Enable();
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		InputAction.FindActionMap("Player").Enable();
 
 		_moveAction = InputAction.FindActionMap("Player").FindAction("Move");
 		_jumpAction = InputAction.FindActionMap("Player").FindAction("Jump");
+		_SprintAction = InputAction.FindActionMap("Player").FindAction("Sprint");
 
 		_rb = GetComponent<Rigidbody>();
 		_jumpParticles = GetComponentInChildren<ParticleSystem>();
 		_characterAnimator = GetComponent<Animator>();
-		
+
 		// Lock cursor to center of screen for mouse look
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
+		_isRunning = false;
     }
 
 	// Update is called once per frame
 	void Update()
 	{
-		// transform.rotation = Camera.transform.rotation;
 		transform.rotation = new Quaternion(0, Camera.transform.rotation.y, 0, Camera.transform.rotation.w);
 		if (_jumpAction.WasPressedThisFrame() && _isGrounded && !_characterAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
 			Jump();
@@ -55,23 +59,57 @@ public class PlayerMouvment : MonoBehaviour
 	void FixedUpdate()
 	{
 		_moveInput = _moveAction.ReadValue<Vector2>();
-		Walking();
+		Moving();
 	}
 
-	private void Walking()
+	private void Moving()
 	{
+		Vector3 targetVelocity;
+		if (_SprintAction.IsPressed())
+		{
+			_isRunning = true;
+			targetVelocity = transform.forward * _moveInput.y * _runSpeed +
+				transform.right * _moveInput.x * _runSpeed;
+			_characterAnimator.SetBool("IsRunning", true);
+		}
+		else
+		{
+			_isRunning = false;
+			targetVelocity = transform.forward * _moveInput.y * _moveSpeed +
+				transform.right * _moveInput.x * _moveSpeed;
+			_characterAnimator.SetBool("IsRunning", false);
+		}
+		if (_moveInput.y < 0f)
+		{
+			_isRunning = false; // Stop running if moving backward
+			targetVelocity = transform.forward * _moveInput.y * _moveSpeed +
+				transform.right * _moveInput.x * _moveSpeed;
+			_characterAnimator.SetBool("IsRunning", false);
+		}
 		// Calculate target velocity based on input
-		Vector3 targetVelocity = transform.forward * _moveInput.y * _moveSpeed + 
-								 transform.right * _moveInput.x * _moveSpeed;
-		
+
+
 		// Smoothly interpolate current velocity towards target velocity
 		Vector3 smoothedVelocity = Vector3.Lerp(_rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * _movementSmoothing);
 
-		if (_moveInput.magnitude < 0.1f) {
+		// Bool RunType : 0 - RunForward, 1 - RunForwardLeft, 2 - RunForwardRight, 3 - RunLeft, 4 - RunRight
+		_characterAnimator.SetInteger("RunType", 0); 
+		if (_moveInput.y > 0.1f && _moveInput.x < -0.1f)
+			_characterAnimator.SetInteger("RunType", 1);
+		else if (_moveInput.y > 0.1f && _moveInput.x > 0.1f)
+			_characterAnimator.SetInteger("RunType", 2); 
+		else if (_moveInput.y > -0.1f && _moveInput.y < 0.1f && _moveInput.x < -0.1f)
+			_characterAnimator.SetInteger("RunType", 3);
+		else if (_moveInput.y > -0.1f && _moveInput.y < 0.1f && _moveInput.x > 0.1f)
+			_characterAnimator.SetInteger("RunType", 4);
+
+		if (_moveInput.magnitude < 0.1f)
+		{
 			smoothedVelocity = Vector3.zero; // Stop movement if input is very low
 			_characterAnimator.SetTrigger("Idle");
 		}
-		else {
+		else if (!_isRunning)
+		{
 			_characterAnimator.SetTrigger("WalkForward");
 		}
 
