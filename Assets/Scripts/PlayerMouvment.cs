d813e3ab197e0f17d834bb5695ecaeebb80b9655
Baba <45125301+basilounet@users.noreply.github.com>
2025-07-03 19:58:05 +0200

    Added bodies & animations to workers

--- Assets/Scripts/PlayerMouvment.cs
+++ Assets/Scripts/PlayerMouvment.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerMouvment : MonoBehaviour
 {
+	private static readonly int WalkForward = Animator.StringToHash("WalkForward");
 
-    [SerializeField] private GameObject			Camera;
+	[SerializeField] private GameObject			Camera;
     [SerializeField] private InputActionAsset	InputAction;
 	[SerializeField] private Animator 			_characterAnimator;
 
@@ -64,27 +65,22 @@ public class PlayerMouvment : MonoBehaviour
 
 	private void Moving()
 	{
-		Vector3 targetVelocity;
-		if (_SprintAction.IsPressed())
+		Vector3 targetVelocity = default;
+		if (_SprintAction.IsPressed() && _moveInput.y >= 0f)
 		{
 			_isRunning = true;
-			targetVelocity = transform.forward * _moveInput.y * _runSpeed +
-				transform.right * _moveInput.x * _runSpeed;
+			targetVelocity = transform.forward * (_moveInput.y * _runSpeed) +
+				transform.right * (_moveInput.x * _runSpeed);
 			_characterAnimator.SetBool("IsRunning", true);
 		}
 		else
 		{
 			_isRunning = false;
-			targetVelocity = transform.forward * _moveInput.y * _moveSpeed +
-				transform.right * _moveInput.x * _moveSpeed;
+			targetVelocity = transform.forward * (_moveInput.y * _moveSpeed) +
+				transform.right * (_moveInput.x * _moveSpeed);	
 			_characterAnimator.SetBool("IsRunning", false);
-		}
-		if (_moveInput.y < 0f)
-		{
-			_isRunning = false; // Stop running if moving backward
-			targetVelocity = transform.forward * _moveInput.y * _moveSpeed +
-				transform.right * _moveInput.x * _moveSpeed;
-			_characterAnimator.SetBool("IsRunning", false);
+			if (_moveInput.y > 0.1f || _moveInput.y < -0.1f)
+				_characterAnimator.SetTrigger(WalkForward);
 		}
 		// Calculate target velocity based on input
 
@@ -93,7 +89,7 @@ public class PlayerMouvment : MonoBehaviour
 		Vector3 smoothedVelocity = Vector3.Lerp(_rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * _movementSmoothing);
 
 		// Bool RunType : 0 - RunForward, 1 - RunForwardLeft, 2 - RunForwardRight, 3 - RunLeft, 4 - RunRight
-		_characterAnimator.SetInteger("RunType", 0); 
+		_characterAnimator.SetInteger("RunType", 0);
 		if (_moveInput.y > 0.1f && _moveInput.x < -0.1f)
 			_characterAnimator.SetInteger("RunType", 1);
 		else if (_moveInput.y > 0.1f && _moveInput.x > 0.1f)
--- Assets/Scripts/ai.cs
+++ Assets/Scripts/ai.cs
@@ -6,9 +6,13 @@ using UnityEngine.Splines;
 
 public class ai : MonoBehaviour
 {
+	private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+	private static readonly int Idle = Animator.StringToHash("Idle");
 
 	[SerializeField] private GameObject chair;
+	 private Animator 			_characterAnimator;
 
+	
 	private NavMeshAgent _nma;
 	private Boolean _chairReached;
 	private Vector3 _lastChairPos;
@@ -20,25 +24,31 @@ public class ai : MonoBehaviour
 		_chairReached = false;
 		_nma = GetComponent<NavMeshAgent>();
 		_lastChairPos = chair.transform.position + new Vector3(1, 1, 1);
+		_characterAnimator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Vector3.Distance(transform.position, chair.transform.position) > 2)
+		if (Vector3.Distance(transform.position, chair.transform.position) > 3.5) {
 			_chairReached = false;
+			_characterAnimator.SetBool(IsWalking, true);
+		}
 		else
 		{
-			if (!_chairReached)
+			if (!_chairReached) {
 				_nma.SetDestination(transform.position);
+			}
 			_chairReached = true;
+			_characterAnimator.SetBool(IsWalking, false);
+			_characterAnimator.SetTrigger(Idle);
 		}
 		if (!_chairReached && chair.transform.position != _lastChairPos)
 		{
 			_nma.SetDestination(chair.transform.position);
 		}
-		Debug.Log(Vector3.Distance(transform.position, chair.transform.position));
-		Debug.Log(_nma.destination);
+		// Debug.Log(Vector3.Distance(transform.position, chair.transform.position));
+		// Debug.Log(_nma.destination);
 	}
 
 
