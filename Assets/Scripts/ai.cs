b6dc99f3e8879591c04f4856a48fadd1dab71464
Manualouest <manutea.birou@gmail.com>
2025-07-03 18:09:55 +0200

    added mini preset of behiavours need to do them

--- Assets/Scripts/ai.cs
+++ Assets/Scripts/ai.cs
@@ -1,4 +1,6 @@
 using System;
+using Mono.Cecil;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,25 +8,64 @@ using UnityEngine.Splines;
 
 public class ai : MonoBehaviour
 {
+	private struct Increment { public float min, max, time, delta; };
 
 	[SerializeField] private GameObject chair;
 
 	private NavMeshAgent _nma;
 	private Boolean _chairReached;
-	private Vector3 _lastChairPos;
-
+	private Boolean _onDesk;
+	private Vector3 _lastTargetPos;
+	private Increment _madnessIncr;
+	public float _madness;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
 		_chairReached = false;
+		_onDesk = false;
 		_nma = GetComponent<NavMeshAgent>();
-		_lastChairPos = chair.transform.position + new Vector3(1, 1, 1);
+		_lastTargetPos = chair.transform.position + new Vector3(1, 1, 1);
+		_madnessIncr = new Increment { min = 0, max = 0.5f, time = 0, delta = 2 };
+		_madness = 0;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		_madness += HandleMadnessIncr();
+
+		if (_madness <= 20)
+			NormalBehaviour();
+		else if (_madness <= 45)
+			unBotheredBehaviour();
+		else
+		{
+			_onDesk = false;
+			_chairReached = false;
+			if (_madness <= 70)
+				DerangedBehaviour();
+			else if (_madness <= 105)
+				CrazyBehaviour();
+			else if (_madness <= 140)
+				InsaneBehaviour();
+		}
+	}
+
+	private float HandleMadnessIncr()
+	{
+		_madnessIncr.time += Time.deltaTime;
+		if (_madnessIncr.time >= _madnessIncr.delta)
+		{
+			_madnessIncr.time = 0;
+			return (UnityEngine.Random.Range(_madnessIncr.min, _madnessIncr.max));
+		}
+		return (0);
+	}
+
+	private void NormalBehaviour()
+	{
+		_madnessIncr = new Increment { min = 0.2f, max = 0.5f, time = _madnessIncr.time, delta = 2 };
 		if (Vector3.Distance(transform.position, chair.transform.position) > 2)
 			_chairReached = false;
 		else
@@ -33,20 +74,47 @@ public class ai : MonoBehaviour
 				_nma.SetDestination(transform.position);
 			_chairReached = true;
 		}
-		if (!_chairReached && chair.transform.position != _lastChairPos)
+		if (!_chairReached && chair.transform.position != _lastTargetPos)
 		{
 			_nma.SetDestination(chair.transform.position);
+			if (!_onDesk)
+			{
+				_onDesk = true;
+				//# get on desk
+				//# startwork
+			}
 		}
-		Debug.Log(Vector3.Distance(transform.position, chair.transform.position));
-		Debug.Log(_nma.destination);
 	}
 
+	private void unBotheredBehaviour()
+	{
+		_madnessIncr = new Increment { min = 0.3f, max = 0.75f, time = _madnessIncr.time, delta = 2 };
+		//#stopwork
+		//#on desk animations
+	}
+	private void DerangedBehaviour()
+	{
+		_madnessIncr = new Increment { min = 0.5f, max = 1, time = _madnessIncr.time, delta = 1.5f };
+		_onDesk = false;
+		_chairReached = false;
+		//# get out of desk
+		//# starts walking around
 
-	// private void OnTriggerEnter(Collider objectCollided)
-	// {
-	// 	if (objectCollided.GameObject() == chair)
-	// 	{
-	// 		_chairReached = true;
-	// 	}
-	// }
+	}
+	private void CrazyBehaviour()
+	{
+		_madnessIncr = new Increment { min = 0.75f, max = 1.25f, time = _madnessIncr.time, delta = 1.5f };
+		//# starts running around
+		_onDesk = false;
+		_chairReached = false;
+
+	}
+	private void InsaneBehaviour()
+	{
+		_madnessIncr = new Increment { min = 1, max = 1.5f, time = _madnessIncr.time, delta = 1.5f };
+		//# starts tweaking
+		_onDesk = false;
+		_chairReached = false;
+
+	}
 }
