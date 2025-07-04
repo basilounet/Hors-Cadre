77314fda74ee5121c07b4dfe7f42ceaf3fcf3ff1
Denis <rey.denis@hotmail.com>
2025-07-04 02:31:19 +0200

    polish

--- Assets/Scripts/buttonPause.cs
+++ Assets/Scripts/buttonPause.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
 
-public class buttonPause : MonoBehaviour
+public class PauseManager : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void Pause() 
-    { 
-        pauseMenu.SetActive(true);
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        // Pour quitter le jeu (ne marche pas dans l’éditeur)
+        Application.Quit();
     }
 }
